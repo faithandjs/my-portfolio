90053fae6d693cb34128bacf7cecd83f5fde393b
debaaah <faiththe1rst@example.com>
2022-03-25 23:57:40 +0100

    update

--- src/components/home.js
+++ src/components/home.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './home.css'
+import play from './images/play.png'
 import code from './images/code.png'
 import react from './images/react.png'
 import bootstrap from './images/bootstrap.png'
@@ -8,15 +9,31 @@ import css from './images/css.png'
 import js from './images/js.png'
 import phone from './images/phone.png'
 import linkedin from './images/linkedin.png'
-import twitter from './images/twitter.png'
+import github from './images/github.png'
 import email from './images/email.PNG'
-import restaurant from './images/restaurant.jpg' 
-import restaurant1 from './images/restaurant1.jpg'
-import restaurant2 from './images/restaurant2.jpg'
-import restaurant3 from './images/restaurant3.jpg'
+import restaurant from './images/restaurant11.jpg' 
+import ics from './images/interactive-cs1.jpg'
+import ip from './images/ip-address-tracker1.jpg'
+import portfolio from './images/portfolio1.jpg'
 import './sass/style.scss'
 
 export const Home = () => {
+    const [data, setData] = useState( require('./data.json'))
+    const [datum, setDatum] = useState({title:'', img:[], text:'', github:'', live:'', target:''})
+    const settingDatum = (n) => {
+        const {name, img, text, github, live, target} = data.projects[n]
+        setDatum({ title: name, img, text, github, live, target})
+    }
+    const carousel = (item, index) => {
+        let classes;
+        if (index === 0){classes = "carousel-item active"}
+        else {classes = "carousel-item"}
+        console.log(classes)
+            return <div className={classes}>
+                        <img src={item[index]} className="d-block w-100" alt="preview of my restaurants website" />
+                    </div>
+    }
+    console.log('datum', datum.img)
     return (
         <div className='body'>
             <section className='introduction'>
@@ -31,12 +48,11 @@ export const Home = () => {
                 <div className='about-text'>Hey there! My name is Okogbo Faith and I 
                 am a  Nigeria-based frontend developer. I had my first coding experience 
                 in December 2020 and it has been a wonderful experience so far. 
-                As a sociable person, I have been able to connect with other developers 
-                that have been able to learn from and to mentor me in my journey. Being 
-                a programmer means a lot of learning, practicing and building and I am
-                 enjoying improving myself as a developer. Most of the projects I have 
-                 worked on in the course of my journey can be found on my 
-                 <a href='https://github.com/debaaah' target='_blank'>github</a>.
+                As a self-taught developer, I have prioritised connecting with other developers to 
+                to check notes with and keep me on the right track and so far, taht has been my most reliable 
+                way of learning. Being a programmer means a lot of learning, practicing and building and I am
+                 enjoying this journey of self improvement. Thank you for checking out my portfolio, you can check out
+                 my projects on <a href='https://github.com/debaaah' target='_blank'>github</a>.
                 </div>
 
                 <div className='skills'>
@@ -64,20 +80,40 @@ export const Home = () => {
                         </li>
                     </ul>
                 </div>
-                <div className='about-text'>I am open to internship or entry level frontend development jobs. Thanks for getting to know me, <a href='#contact'>do reach out</a>.</div>
+                <div className='about-text'>I am currently searching for internship opportunities or entry level jobs thank you and <a href='#contact'>do reach out</a>.</div>
             </section>
 
             <section className='work'>
-                <h4>My work</h4>
+                <h3>My work</h3>
                 <div className='gallery'>
-                    <div className='img-box' data-toggle="modal" data-target="#modal">
+                    <div className='img-box' data-toggle="modal" onClick={() => {
+                         settingDatum(0)
+                    }} data-target="#modal">
                         <img src={restaurant} />
                     </div>
+                    <div className='img-box' data-toggle="modal" onClick={() => {
+                         settingDatum(1)
+                    }} data-target="#modal">
+                        <img src={ics} />
+                    </div>
+                    <div className='img-box' data-toggle="modal" onClick={() => {
+                         settingDatum(2)
+                    }} data-target="#modal">
+                        <img src={ip} />
+                    </div>
+                    <div className='img-box' data-toggle="modal" onClick={() => {
+                         settingDatum(3)
+                    }} data-target="#modal">
+                        <img src={portfolio} />
+                    </div>
+                   
+                </div>
+                <div className='extras'>
                     <div className='see-more'>
                         <a href='https://github.com/debaaah?tab=repositories' target='_blank'>More</a>
                     </div>
-                </div>
-                <div className='btn'><a href='okogbo-onosedeba-cv.pdf' download>download cv</a></div>
+                    <div className='btn'><a href='okogbo-onosedeba-cv.pdf' download>Download CV</a></div>
+                </div> 
             </section>
             <div className='spacer layer1'></div>
             <section className='contact' id='contact'>
@@ -105,18 +141,15 @@ export const Home = () => {
                         <div className="modal-body">
                             <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
                                 <div className="carousel-inner">
-                                    <div className="carousel-item active">
-                                        <img src={restaurant} className="d-block w-100" alt="preview of my restaurants website" />
-                                    </div>
-                                    <div className="carousel-item">
-                                        <img src={restaurant1} className="d-block w-100" alt="preview of my restaurants website" />
-                                    </div>
-                                    <div className="carousel-item">
-                                        <img src={restaurant2} className="d-block w-100" alt="preview of my restaurants website" />
-                                    </div>
-                                    <div className="carousel-item">
-                                        <img src={restaurant3} className="d-block w-100" alt="preview of my restaurants website" />
-                                    </div>
+                                    {datum.img.map((item, index) => {
+                                        
+                                        let classes;
+                                        if (index === 0){classes = "carousel-item active"}
+                                        else {classes = "carousel-item"}
+                                        console.log(classes)
+                                        return <div className={classes}>
+                                                        <img src={item[index]} className="d-block w-100" alt="preview of my restaurants website" />
+                                                    </div>})}
                                 </div>
                                 <button className="carousel-control-prev" type="button" data-target="#carouselExampleControls" data-slide="prev">
                                     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -129,10 +162,12 @@ export const Home = () => {
                             </div>
                         </div>
                         <div className="modal-footer">
-                            <div className='caption'>This is a website for a restaurant that I designed and developed. It was made with React Js, CSS and Bootstrap</div>
+                            <div className='caption'>{datum.text}</div>
                             <div className='footer-btns'>
-                                <a href='https://github.com/debaaah/a-restaurants-website' target='_blank'><button type="button" className="btn">Github</button></a>
-                                <button type='button' className='btn close-modal' data-dismiss='modal'>x</button>
+                               <a href={datum.github} target='_blank'><div><img src={github} /></div></a>
+                                <a href={datum.live} target={datum.target}><div><img src={play} /></div></a>
+                                
+                               {/* <button type='button' className='btn close-modal' data-dismiss='modal'>x</button>*/}
                             </div>
                         </div>
                     </div>
